44763d33bff07d5a21cd989da6435d0ec48666da
akhgang14 <akhgang14@gmail.com>
2026-09-19 09:43:33 +0530

    Undo CORS

--- backend/main.py
+++ backend/main.py
@@ -11,11 +11,15 @@ from api import document
 
 app = FastAPI(title="CivicBridge API")
 
+frontend_url=os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:3000"
+)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://civicbridge-frontend-seven.vercel.app",
+        frontend_url
     ],
     allow_credentials=True,
     allow_methods=["*"],
